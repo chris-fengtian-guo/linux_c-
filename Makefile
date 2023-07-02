0c63fddb2d182d180c85dcfbea32177e21846138
Your Name <you@example.com>
2023-07-02 20:58:30 +0800

    update
    Signed-off-by: Your Name <you@example.com>

---
+++ Makefile
@@ -0,0 +1,21 @@
+# the compiler to use.
+CC = g++
+
+# compiler flags:
+#  -Wall turns on most, but not all, compiler warnings
+#  -std=c++11 to use the C++11 standard
+CFLAGS  = -Wall -std=c++11
+
+# libraries to link into executable:
+LIBS = -lboost_system -lrt
+
+# the name to use for both the target source file, and the output file:
+TARGET = shared_memory
+
+all: $(TARGET)
+
+$(TARGET): $(TARGET).cpp
+	$(CC) $(CFLAGS) -o $(TARGET) $(TARGET).cpp $(LIBS)
+
+clean:
+	$(RM) $(TARGET)
---
+++ shared_memory.cpp
@@ -0,0 +1,34 @@
+#include <boost/interprocess/shared_memory_object.hpp>
+#include <boost/interprocess/mapped_region.hpp>
+#include <algorithm> // for std::count_if
+#include <iostream>
+
+int main()
+{
+    using namespace boost::interprocess;
+
+    shared_memory_object shdmem{open_or_create, "Boost", read_write};
+    shdmem.truncate(1024);
+    mapped_region region{shdmem, read_write};
+
+    int *mem = static_cast<int*>(region.get_address());
+    std::size_t size = region.get_size() / sizeof(int);
+
+    for(std::size_t i = 0; i < size; ++i)
+    {
+        mem[i] = 1;
+    }
+
+    if(std::count_if(mem, mem + size, [](int x){ return x == 1; }) == static_cast<std::ptrdiff_t>(size))
+    {
+        std::cout << "The memory was correctly initialized\n";
+    }
+    else
+    {
+        std::cout << "The memory was not correctly initialized\n";
+    }
+    
+    shared_memory_object::remove("Boost");
+
+    return 0;
+}
