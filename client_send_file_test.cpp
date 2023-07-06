a6b0a8802df2232949496e9ddb7f79d7a8925e85
Your Name <you@example.com>
2023-07-06 11:05:31 +0800

    update
    Signed-off-by: Your Name <you@example.com>

--- client_send_file_test.cpp
+++ client_send_file_test.cpp
@@ -29,7 +29,14 @@ std::string get_file_md5(const std::string& file_name) {
 }
 
 void receive_file(tcp::socket& socket, const std::string& file_name) {
-    std::ofstream file(file_name, std::ios::binary);
+    // Truncate the file if it already exists, create it if it does not.
+    std::ofstream file(file_name, std::ios::binary | std::ios::trunc);
+
+    // Check if the file was opened correctly.
+    if (!file.is_open() || file.fail()) {
+        std::cerr << "Failed to open the file.\n";
+        return;
+    }
 
     // First receive the MD5 from the server.
     std::vector<char> md5(32);
@@ -38,13 +45,34 @@ void receive_file(tcp::socket& socket, const std::string& file_name) {
     // Then receive the file data.
     std::vector<char> file_data(1024);
     size_t length;
-    while ((length = socket.read_some(boost::asio::buffer(file_data))) > 0) {
-        file.write(file_data.data(), length);
+    try {
+        while ((length = socket.read_some(boost::asio::buffer(file_data))) > 0) {
+            file.write(file_data.data(), length);
+            
+            // Check if the write was successful.
+            if (file.fail()) {
+                std::cerr << "Failed to write to the file.\n";
+                return;
+            }
+        }
+    } catch (const boost::system::system_error& ex) {
+        // If we get an EOF error, it's expected, so don't report it.
+        if (ex.code() != boost::asio::error::eof) {
+            std::cerr << "Read error: " << ex.what() << "\n";
+        }
     }
 
     // Verify the MD5 of the received file.
     std::string received_md5 = get_file_md5(file_name);
-    if (received_md5 == std::string(md5.begin(), md5.end())) {
+
+    // Print the received MD5 from server.
+    std::string server_md5 = std::string(md5.begin(), md5.end());
+    std::cout << "MD5 from server: " << server_md5 << std::endl;
+
+    // Print the calculated MD5.
+    std::cout << "Calculated MD5 of received file: " << received_md5 << std::endl;
+
+    if (received_md5 == server_md5) {
         std::cout << "File received correctly.\n";
     } else {
         std::cerr << "File received with errors.\n";
@@ -61,7 +89,7 @@ int main() {
         tcp::socket socket(io_service);
         boost::asio::connect(socket, endpoint_iterator);
 
-        receive_file(socket, "received_file.bin");  // Replace with your file path.
+        receive_file(socket, "new_received_file.bin");  // Replace with your file path.
     } catch (std::exception& e) {
         std::cerr << "Exception: " << e.what() << "\n";
     }
