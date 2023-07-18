4736318968be5c75c3df4a64ea216d4a6dd7cbb3
Your Name <you@example.com>
2023-07-18 18:12:02 +0800

    udp server/client 的异步收发-并且支持接受完数据-发送回执
    Signed-off-by: Your Name <you@example.com>

--- message_process/udp_client.cpp
+++ message_process/udp_client.cpp
@@ -8,18 +8,13 @@ using boost::asio::ip::udp;
 const int PACKET_SIZE = 1024;
 const int TOTAL_DATA_SIZE = 12000;  // Total size of the data to be sent
 
-int main(int argc, char* argv[]) {
-    try {
-        if (argc != 3) {
-            std::cerr << "Usage: client <host> <port>\n";
-            return 1;
-        }
-
-        boost::asio::io_service io_service;
+class UDPClient {
+public:
+    UDPClient(boost::asio::io_service& io_service, const std::string& host, const std::string& port)
+        : socket_(io_service) {
         udp::resolver resolver(io_service);
-        udp::endpoint receiver_endpoint = *resolver.resolve({udp::v4(), argv[1], argv[2]});
-        udp::socket socket(io_service);
-        socket.open(udp::v4());
+        udp::endpoint receiver_endpoint = *resolver.resolve({udp::v4(), host, port});
+        socket_.open(udp::v4());
 
         std::string data(TOTAL_DATA_SIZE, 'x');  // Create a string of 12000 characters
         std::vector<char> send_buffer;
@@ -39,9 +34,43 @@ int main(int argc, char* argv[]) {
         for (size_t i = 0; i < send_buffer.size(); i += PACKET_SIZE) {
             std::vector<char> packet(send_buffer.begin() + i, 
                 send_buffer.begin() + std::min(send_buffer.size(), i + PACKET_SIZE));
-            socket.send_to(boost::asio::buffer(packet), receiver_endpoint);
+            socket_.send_to(boost::asio::buffer(packet), receiver_endpoint);
         }
 
+        // Start asynchronous receive for receipt from the server
+        start_receive();
+    }
+
+private:
+    void start_receive() {
+	    std::cout << "received receipt\n";
+        socket_.async_receive_from(
+            boost::asio::buffer(recv_buffer_), remote_endpoint_,
+            [this](boost::system::error_code ec, std::size_t bytes_transferred) {
+                if (!ec) {
+                    std::string receipt(recv_buffer_.data(), bytes_transferred);
+                    std::cout << "Receipt: " << receipt << std::endl;
+                }
+                socket_.close();
+            });
+    }
+
+    udp::socket socket_;
+    udp::endpoint remote_endpoint_;
+    std::array<char, 128> recv_buffer_;
+};
+
+int main(int argc, char* argv[]) {
+    try {
+        if (argc != 3) {
+            std::cerr << "Usage: client <host> <port>\n";
+            return 1;
+        }
+
+        boost::asio::io_service io_service;
+        UDPClient client(io_service, argv[1], argv[2]);
+
+        io_service.run();
     } catch (std::exception& e) {
         std::cerr << "Exception: " << e.what() << "\n";
     }
--- message_process/udp_server.cpp
+++ message_process/udp_server.cpp
@@ -8,9 +8,9 @@ const int HEADER_SIZE = 4;  // 4 bytes = 32 bits
 
 class UDPServer {
 public:
-    UDPServer(boost::asio::io_service& io_service, short port)
+    UDPServer(boost::asio::io_service& io_service, short port, bool receipt_mode = false)
         : socket_(io_service, udp::endpoint(udp::v4(), port)),
-          total_size_(0), received_size_(0) {
+          total_size_(0), received_size_(0), receipt_mode_(receipt_mode) {
         start_receive();
     }
 
@@ -24,19 +24,33 @@ private:
                         // Parse the header to get the total size
                         total_size_ = *reinterpret_cast<int*>(recv_buffer_.data());
                         received_size_ += bytes_transferred - HEADER_SIZE;
-			std::cout << "init receive bytes=" << bytes_transferred << "\n";
                     } else {
                         received_size_ += bytes_transferred;
-			std::cout << "middle receive bytes=" << bytes_transferred << "\n";
                     }
 
                     if (received_size_ >= total_size_) {
                         std::cout << "Received total data of size: " << received_size_ << std::endl;
-                        socket_.close();
+                        if (receipt_mode_) {
+			    std::cout << "receipt_mode enable \n";
+                            send_receipt();
+                        } else {
+			    std::cout << "not receipt close socket\n";
+                            socket_.close();
+                        }
                     } else {
                         start_receive();
                     }
                 }
+            });
+    }
+
+    void send_receipt() {
+        std::string receipt = "Received: " + std::to_string(received_size_) + " Sent: " + std::to_string(total_size_);
+	std::cout << "send receipt =" << receipt << "\n";
+        socket_.async_send_to(
+            boost::asio::buffer(receipt), remote_endpoint_,
+            [this](boost::system::error_code /*ec*/, std::size_t /*bytes_transferred*/) {
+                socket_.close();
             });
     }
 
@@ -45,17 +59,18 @@ private:
     std::array<char, 65536> recv_buffer_;  // max UDP size
     int total_size_;
     int received_size_;
+    bool receipt_mode_;
 };
 
 int main(int argc, char* argv[]) {
     try {
-        if (argc != 2) {
-            std::cerr << "Usage: server <port>\n";
+        if (argc != 3) {
+            std::cerr << "Usage: server <port> <receipt_mode>\n";
             return 1;
         }
 
         boost::asio::io_service io_service;
-        UDPServer server(io_service, std::stoi(argv[1]));
+        UDPServer server(io_service, std::stoi(argv[1]), std::stoi(argv[2]) != 0);
 
         io_service.run();
     } catch (std::exception& e) {
